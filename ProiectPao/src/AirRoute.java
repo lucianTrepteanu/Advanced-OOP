eef5cec4294d6031fe5d830bac9b65015ab76f11
unknown <lucian.trepteanu@gmail.com>
2020-08-21 18:34:30 +0300

    cleaned

--- ProiectPao/src/AirRoute.java
+++
@@ -1,5 +0,0 @@
-public class AirRoute extends Route {
-    public AirRoute(String fDest, String sDest, double distance, int time, double moneyCost) {
-        super(fDest, sDest, distance, time, moneyCost);
-    }
-}
--- ProiectPao/src/GroundRoute.java
+++
@@ -1,5 +0,0 @@
-public class GroundRoute extends Route{
-    public GroundRoute(String fDest, String sDest, double distance, int time, double moneyCost) {
-        super(fDest, sDest, distance, time, moneyCost);
-    }
-}
